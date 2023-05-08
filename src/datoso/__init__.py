404f16fdce833e230dc3f41ac050d6b532e65636
laromicas <laromicas@hotmail.com>
2023-05-08 17:46:34 -0500

    Bumped version

--- src/datoso/__init__.py
+++ src/datoso/__init__.py
@@ -3,7 +3,7 @@ __init__.py
 """
 import os
 # Version of the datoso package
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __app_name__ = "datoso"
 
 ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))
