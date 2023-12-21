9a51098f81b862f5dfc7a2005953f6351d8c1dec
laromicas <laromicas@hotmail.com>
2023-12-21 10:45:30 -0500

    - Some fixes for clrmame dat reader - Some changes to directory handling to work better with windows

--- src/datoso/__init__.py
+++ src/datoso/__init__.py
@@ -3,7 +3,7 @@ __init__.py
 """
 import os
 # Version of the datoso package
-__version__ = "0.3.9"
+__version__ = "0.3.10"
 __app_name__ = "datoso"
 
 ROOT_FOLDER = os.path.dirname(os.path.abspath(__file__))
