486bb1eb6d4e2149ab78fd87a08062c7d8d5453d
laromicas <laromicas@hotmail.com>
2024-06-14 22:39:19 -0500

    Update version

--- src/datoso/__init__.py
+++ src/datoso/__init__.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 # Version of the datoso package
-__version__ = '1.0b0.dev0'
+__version__ = '1.0.0rc1'
 __app_name__ = 'datoso'
 
 ROOT_FOLDER = Path(__file__).resolve().parent
